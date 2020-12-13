9206c2f0a74b9f7fa0afeb1d13c922a665cd97c4
zoe <zoster@users.noreply.github.com>
2020-12-13 18:36:14 -0500

    it's a custom block now

--- custom.ts
+++ custom.ts
@@ -1,1 +1,56 @@
+namespace blair {
+    /**
+    * Computes the famous Fibonacci number sequence!
+    */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value - 1) + fib(value - 2);
+    }
 
+    /**
+    * Plays the Star Wars theme
+    */ 
+    //% block
+    export function starWarsTheme(times: number)  {
+        if (times < 1 ) {
+            times = 1;
+        }
+        for (let index = 0; index < times; index++) {
+            music.rest(music.beat(BeatFraction.Whole))
+            music.playTone(196, music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Eighth))
+            music.playTone(196, music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Eighth))
+            music.playTone(196, music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Eighth))
+            music.playTone(262, music.beat(BeatFraction.Whole))
+            music.playTone(262, music.beat(BeatFraction.Whole))
+            music.playTone(392, music.beat(BeatFraction.Whole))
+            music.playTone(392, music.beat(BeatFraction.Whole))
+            music.playTone(349, music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Eighth))
+            music.playTone(330, music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Eighth))
+            music.playTone(294, music.beat(BeatFraction.Quarter))
+            music.playTone(523, music.beat(BeatFraction.Whole))
+            music.playTone(523, music.beat(BeatFraction.Whole))
+            music.playTone(392, music.beat(BeatFraction.Whole))
+            music.playTone(349, music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Eighth))
+            music.playTone(330, music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Eighth))
+            music.playTone(294, music.beat(BeatFraction.Quarter))
+            music.playTone(523, music.beat(BeatFraction.Whole))
+            music.playTone(523, music.beat(BeatFraction.Whole))
+            music.playTone(392, music.beat(BeatFraction.Whole))
+            music.playTone(349, music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Eighth))
+            music.playTone(330, music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Eighth))
+            music.playTone(349, music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Eighth))
+            music.playTone(294, music.beat(BeatFraction.Whole))
+            music.playTone(294, music.beat(BeatFraction.Whole))
+        }
+    }
+}
--- main.ts
+++ main.ts
@@ -1,6 +1,1 @@
-input.onGesture(Gesture.Shake, function () {
-	
-})
-control.inBackground(function () {
-    basic.showString("Darth Vader: The force is strong in that one")
-})
+blair.starWarsTheme(2)
